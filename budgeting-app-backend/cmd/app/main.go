9266276dbe6cc8012eb207783a19fdfe66bcfda6
J-H-Tran <tr.h.jonathan@gmail.com>
2024-07-31 16:13:36 +0900

    local db connection

--- budgeting-app-backend/cmd/app/main.go
+++ budgeting-app-backend/cmd/app/main.go
@@ -4,11 +4,16 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/J-H-Tran/budgeting-app-backend/config"
+	"github.com/J-H-Tran/budgeting-app-backend/internal/routes"
 	"github.com/gorilla/mux"
 )
 
 func main() {
+	config.ConnectDB()
 	r := mux.NewRouter()
+	routes.BudgetRoutes(r)
+
 	r.HandleFunc("/", HomeHandler)
 
 	log.Fatal(http.ListenAndServe(":8080", r))
